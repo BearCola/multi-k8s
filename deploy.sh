2915834d634405abeb163c730c0b5c7723704e93
BearCola <sangin84@gmail.com>
2020-02-25 10:47:02 +0900

    update source

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t diablo1031/multi-client -f ./client/Dockerfile ./client
-docker build -t diablo1031/multi-server -f ./server/Dockerfile ./server
-docker build -t diablo1031/multi-worker -f ./worker/Dockerfile ./worker
-docker push diablo1031/multi-client
-docker push diablo1031/multi-server
-docker push diablo1031/multi-worker
+docker build -t diablo1031/multi-client:latest -t diablo1031/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t diablo1031/multi-server:latest -t diablo1031/multi-client:$SHA -f ./server/Dockerfile ./server
+docker build -t diablo1031/multi-worker:latest -t diablo1031/multi-client:$SHA -f ./worker/Dockerfile ./worker
+
+docker push diablo1031/multi-client:latest
+docker push diablo1031/multi-server:latest
+docker push diablo1031/multi-worker:latest
+
+docker push diablo1031/multi-client:$SHA
+docker push diablo1031/multi-server:$SHA
+docker push diablo1031/multi-worker:$SHA
+
 kubectl apply -f k8s 
-kubectl set image deployments/server-deployment server=diablo1031/multi-server
+kubectl set image deployments/server-deployment server=diablo1031/multi-server:$SHA
+kubectl set image deployments/client-deployment client=diablo1031/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=diablo1031/multi-worker:$SHA
